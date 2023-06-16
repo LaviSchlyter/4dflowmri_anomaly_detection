ca055e44aa7aa1e4cf99b734ad24ea4a0b808d2a
LaviSchlyter <schlyter.lavinia@gmail.com>
2023-06-16 13:12:42 +0200

    add synthetic data, TVAE, MMDVAE

--- train.py
+++ train.py
@@ -12,6 +12,7 @@ import numpy as np
 import datetime
 from config import system as config_sys
 import wandb
+import math
 
 
 # =================================================================================
@@ -20,9 +21,11 @@ import wandb
 
 from helpers import data_bern_numpy_to_preprocessed_hdf5
 from helpers import data_bern_numpy_to_hdf5
-from helpers.utils import make_dir_safely, verify_leakage
+from helpers.utils import make_dir_safely, verify_leakage, kld_min
 from helpers.run import train, load_model, evaluate
-from helpers.data_loader import load_data
+from helpers.data_loader import load_data, load_syntetic_data
+
+
 
 
 
@@ -31,6 +34,8 @@ from helpers.data_loader import load_data
 # =================================================================================
 
 from models.vae import VAE
+from models.tilted_vae import TiltedVAE
+from models.mmd_vae import MMDVAE
 
 # =================================================================================
 # ============== MAIN FUNCTION ==================================================
@@ -55,6 +60,7 @@ if __name__ ==  "__main__":
     parser.add_argument('--do_data_augmentation', type=bool)
     parser.add_argument('--gen_loss_factor', type=float)
     parser.add_argument('--z_dim', type=int)
+    parser.add_argument('--tilt', type=int)
 
     args = parser.parse_args()
 
@@ -66,6 +72,12 @@ if __name__ ==  "__main__":
     for arg, value in vars(args).items():
         if value is not None:
             config[arg] = value
+
+    # ================================================
+    # Check that if self supervised is True, then use_synthetic_validation is also True
+    # ================================================
+    if config['self_supervised']:
+        config['use_synthetic_validation'] = True
     
     # Set device
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -73,15 +85,27 @@ if __name__ ==  "__main__":
     timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M")
 
     if str(config['model_name']) != str(0):
-        
         # Costume name given
         pass
+    if config['model'] == 'vae':
+        config['model_name'] =  f"{timestamp}_{config['model']}_{config['preprocess_method'] + '_SSL' if config['self_supervised'] else config['preprocess_method']}_lr{'{:.3e}'.format(config['lr'])}-e{config['epochs']}-bs{config['batch_size']}-gf_dim{config['gf_dim']}-da{config['do_data_augmentation']}-f{config['gen_loss_factor']}"
+    elif config['model'] == 'tvae':
+        config['model_name'] =  f"{timestamp}_{config['model']}_{config['preprocess_method'] + '_SSL' if config['self_supervised'] else config['preprocess_method']}_lr{'{:.3e}'.format(config['lr'])}-e{config['epochs']}-bs{config['batch_size']}-da{config['do_data_augmentation']}-f{config['gen_loss_factor']}-zdim{config['z_dim']}-tilt{config['tilt']}"
+    elif config['model'] == 'mmd_vae':
+        config['model_name'] =  f"{timestamp}_{config['model']}_{config['preprocess_method'] + '_SSL' if config['self_supervised'] else config['preprocess_method']}_lr{'{:.3e}'.format(config['lr'])}-e{config['epochs']}-bs{config['batch_size']}-gf_dim{config['gf_dim']}-da{config['do_data_augmentation']}-f{config['gen_loss_factor']}-mmdf{config['mmd_loss_factor']}"
     else:
-        config['model_name'] =  f"{timestamp}_{config['preprocess_method']}_lr{'{:.3e}'.format(config['lr'])}-e{config['epochs']}-bs{config['batch_size']}-zdim{config['z_dim']}-da{config['do_data_augmentation']}-f{config['gen_loss_factor']}"
+        config['model_name'] =  f"{timestamp}_{config['model']}_{config['preprocess_method'] + '_SSL' if config['self_supervised'] else config['preprocess_method']}_lr{'{:.3e}'.format(config['lr'])}-e{config['epochs']}-bs{config['batch_size']}-gf_dim{config['gf_dim']}-da{config['do_data_augmentation']}-f{config['gen_loss_factor']}"
     
     wandb_mode = "online" # online/ disabled
+    tag = ''
+    if config['self_supervised']:
+        tag = 'self_supervised'
 
-    with wandb.init(project="4dflowmri_anomaly_detection", name=config['model_name'], config=config, tags= ['debug']):
+    if config['use_synthetic_validation']:
+        tags = [config['model'], 'synthetic_validation', 'debug'].append(tag)
+    else:
+        tags = [config['model'], 'debug'].append(tag)
+    with wandb.init(project="4dflowmri_anomaly_detection", name=config['model_name'], config=config, tags= tags):
         config = wandb.config
         print('after_init', config['model_name'])
 
@@ -112,13 +136,34 @@ if __name__ ==  "__main__":
         verify_leakage()
 
         # Load the data
-        images_tr, images_vl, _ = load_data(config, config_sys, idx_start_tr=0, idx_end_tr=35, idx_start_vl=35, idx_end_vl=42)
+        images_tr, images_vl, _ = load_data(config, config_sys, idx_start_tr=config['idx_start_tr'], idx_end_tr=config['idx_end_tr'], idx_start_vl=config['idx_start_vl'], idx_end_vl=config['idx_end_vl'])
+
+        # Load synthetic data for validation if needed
+        if config['use_synthetic_validation']:
+            images_vl = load_syntetic_data(preprocess_method = config['preprocess_method'], idx_start=config['idx_start_vl'], idx_end=config['idx_end_vl'], sys_config = config_sys)
+            logging.info(f"Using synthetic validation data with shape: {images_vl['images'].shape}")
         
         # ================================================
         # Initialize the model, training parameters, model name and logging
         # ================================================
         if config['model'] == 'vae':
-            model = VAE(z_dim=config['z_dim'], in_channels=4, gf_dim=8).to(device)
+            if config['self_supervised']:
+                # In this case we have a binary classification problem
+                model = VAE(in_channels=4, gf_dim=config['gf_dim'], out_channels=1).to(device)
+            else:
+                model = VAE(in_channels=4, gf_dim=config['gf_dim'], out_channels=4).to(device)
+        elif config['model'] == 'tvae':
+            image_shape = [config['spatial_size_x'], config['spatial_size_y'], config['spatial_size_t'], 4]
+            model = TiltedVAE(shape=image_shape, nz=config['z_dim'], base=32).to(device)
+            # Compute mu_star
+            mu_star = kld_min(config['tilt'], config['z_dim'])
+            if math.isnan(mu_star):
+                raise ValueError(f"mu_star is nan, check the tilt parameter: {config['tilt']}")
+
+            config['mu_star'] = mu_star
+
+        elif config['model'] == 'mmd_vae':
+            model = MMDVAE(in_channels=4, gf_dim=config['gf_dim']).to(device)
         else:
             raise ValueError(f"Unknown model: {config['model']}")
         
