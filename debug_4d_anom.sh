cb0a4538eea141f774e25dd2d7171c71fea5a13b
LaviSchlyter <schlyter.lavinia@gmail.com>
2023-11-09 12:27:34 +0100

    Minor changes

--- debug_4d_anom.sh
+++ debug_4d_anom.sh
@@ -3,8 +3,8 @@
 #SBATCH --output=/scratch_net/biwidl203/lschlyter/jupyter_debug_gpu/logs/TRAIN-%x.%j.out
 #SBATCH --error=/scratch_net/biwidl203/lschlyter/jupyter_debug_gpu/logs/TRAIN-%x.%j.err
 #SBATCH --gres=gpu:1
-#SBATCH --constraint='titan_xp|geforce_rtx_2080_ti|geforce_gtx_1080_ti'
-#SBATCH --account=student
+
+
 
 # Launch the vs-server
 remote.sh
---
+++ inference_bern.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH  --output=sbatch_log_inference/%j.out
+#SBATCH  --gres=gpu:1
+#SBATCH  --mem=50G
+
+source /scratch_net/biwidl203/lschlyter/anaconda3/etc/profile.d/conda.sh
+conda activate vtk_wrap
+python -u visualize_best_model_outputs.py "$@"
