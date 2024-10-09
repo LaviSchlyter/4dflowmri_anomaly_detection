cf90780e9c1fc0a5ed776a68690cc3ec2e2baef1
LaviSchlyter <schlyter.lavinia@gmail.com>
2024-10-09 15:26:53 +0200

    Moved and deleted files

--- inference_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH  --output=sbatch_logs/sbatch_inference_logs/%j.out
-#SBATCH  --gres=gpu:1
-#SBATCH  --mem=50G
-
-source /scratch_net/biwidl203/lschlyter/anaconda3/etc/profile.d/conda.sh
-conda activate anom
-python -u inference_test.py "$@"
--- inference_train.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH  --output=sbatch_logs/sbatch_inference_logs/inference_train/%j.out
-#SBATCH  --gres=gpu:1
-#SBATCH  --mem=50G
-
-source /scratch_net/biwidl203/lschlyter/anaconda3/etc/profile.d/conda.sh
-conda activate anom
-python -u inference_train.py "$@"
